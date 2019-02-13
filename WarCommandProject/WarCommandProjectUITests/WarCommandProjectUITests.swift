abb3884caeac306357d951b8b58d19d064b9088f
Aldren <Aldren_munoz@hotmail.com>
2019-02-13 16:26:29 -0500

    Created the game project

---
+++ WarCommandProject/WarCommandProjectUITests/WarCommandProjectUITests.swift
@@ -0,0 +1,36 @@
+//
+//  WarCommandProjectUITests.swift
+//  WarCommandProjectUITests
+//
+//  Created by Munoz Aldren J. on 2019-02-13.
+//  Copyright © 2019 AJKevBas. All rights reserved.
+//
+
+import XCTest
+
+class WarCommandProjectUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
